45425e0e2100edfc2ea13465901dc81dcbba8b2c
DanielJRaine <ley.danielj@gmail.com>
2016-09-30 14:59:14 -0400

    Add Bootstrap template for navbar
    Download and add Bootstrap template.  Placed relevant files in styles folder.

---
+++ data/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let score = `X: 1
+T: Dusky Night
+S: Thompson 1777
+N: From Apted Book of Country Dances
+N: abc version by Robert Bley-Vroman vroman@hawaii.edu 14 October 1996
+M: 6/8
+L: 1/8
+K:D
+|: A | d2d A2G  | F>EF D2A | B2A G2F | F3   E2E  |
+       A2 A B2B | c>Bc A2A | d2d e2e | f>ef d2F |
+      B>cd e>fg | A>Bc d2A | B2B A>GA | D3-D2  :|
+|: F/D/ | A2A  A2A | A3-A2  B/A/ | d2d d>ef | d3-d2 A |
+          B>cd e>fg| A>Bc d2A | B2G A2A  | D3-D2 :|`;
