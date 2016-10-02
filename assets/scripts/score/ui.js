f7db1012edaa1f96e82dc3a488d956e11a130dba
DanielJRaine <ley.danielj@gmail.com>
2016-10-01 20:20:10 -0400

    Make responsive using ABCJS library engraverParams
    Finally figured out that I needed to pass renderABC function options as hashes with specifically named keys which can be found in their documentation at: https://github.com/paulrosen/abcjs/blob/master/api.md
    
    ABCJS.renderAbc(output, tunebookString, parserParams, engraverParams, renderParams)
    
    ABCJS.renderAbc('dom-element-name', 'abc text goes here', '', {staffwidth: 200});

--- assets/scripts/score/ui.js
+++ assets/scripts/score/ui.js
@@ -2,9 +2,12 @@
 const app = require('../app.js');
 
 const displayScore = (data) => {
-  app.tune = data.tune
-  ABCJS.renderAbc('score-display-2', app.tune.ABCnotation);
-  console.log(data);
+  app.tune = data.tune;
+  // ABCJS.renderAbc(output, tunebookString, parserParams, engraverParams, renderParams)
+  // see https://github.com/paulrosen/abcjs/blob/master/api.md for documentation
+  ABCJS.renderAbc('score-display-1', app.tune.ABCnotation, '', {staffwidth: 650});
+  console.log(app.tune);
+  
 };
 
 const failure = (error) => {
