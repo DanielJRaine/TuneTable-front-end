a4b151c15167725d59da14c3df473e1f3e3c11d3
DanielJRaine <ley.danielj@gmail.com>
2016-10-02 14:00:11 -0400

    Load login upon register. Refactor auth events.
    Broke up event handlers into separate functions so I could reuse them.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -4,29 +4,33 @@ const getFormFields = require('../../../lib/get-form-fields.js');
 const api = require('./api.js');
 const ui = require('./ui.js');
 
+const loadRegisterForm = function(e) {
+  $("#register-form").delay(100).fadeIn(100);
+  $("#login-form").fadeOut(100);
+  $('#login-form-link').removeClass('active');
+  $(this).addClass('active');
+  e.preventDefault();
+};
+
+const loadLoginForm = function(e) {
+  $("#login-form").delay(100).fadeIn(100);
+$("#register-form").fadeOut(100);
+$('#register-form-link').removeClass('active');
+$(this).addClass('active');
+e.preventDefault();
+};
+
 const onRegister = () => {
   event.preventDefault();
   let data = getFormFields(event.target);
   console.log(data);
-  
+  loadLoginForm();
 };
 
 const addHandlers = () => {
   $(function() {
-    $('#login-form-link').click(function(e) {
-      $("#login-form").delay(100).fadeIn(100);
-    $("#register-form").fadeOut(100);
-    $('#register-form-link').removeClass('active');
-    $(this).addClass('active');
-    e.preventDefault();
-  });
-    $('#register-form-link').click(function(e) {
-      $("#register-form").delay(100).fadeIn(100);
-      $("#login-form").fadeOut(100);
-      $('#login-form-link').removeClass('active');
-      $(this).addClass('active');
-      e.preventDefault();
-    });
+    $('#login-form-link').click(loadLoginForm);
+    $('#register-form-link').click(loadRegisterForm);
   });
   $('#register-form').on('submit', onRegister);
 };
