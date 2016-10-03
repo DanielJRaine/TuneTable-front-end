7e463c6c4940d7a7d64b2a63d3dec44d25a5d8e7
DanielJRaine <ley.danielj@gmail.com>
2016-10-03 17:04:40 -0400

    Create automatically incrementing div ID's
    Used Lodash templates and a counter every time the scoreDisplay function is called.
    Next step is to render to these incrementing ID's using ABCJS.renderAbc.

--- assets/scripts/score/ui.js
+++ assets/scripts/score/ui.js
@@ -3,19 +3,26 @@ const app = require('../app.js');
 // const template = require('lodash.template');
 const _ = require('lodash');
 
+let numberOfScoreViews = 0;
 const displayScore = (data) => {
   app.tune = data.tune;
+  numberOfScoreViews += 1;
   // ABCJS.renderAbc(output, tunebookString, parserParams, engraverParams, renderParams)
   // see https://github.com/paulrosen/abcjs/blob/master/api.md for documentation
-
-  let template = _.template("<% ABCJS.renderAbc('score-display-1', ABCnotation, '', {staffwidth: 650}); %>");
+  let musicBoxTemplate = _.template("<svg id='score-display-<%= numberOfScoreViews %>'>I am an SVG</svg>");
+  let compiledMusicBoxTemplate = musicBoxTemplate({
+    numberOfScoreViews: numberOfScoreViews,
+  });
+  $('.music-box').append(compiledMusicBoxTemplate);
   
-    // ABCJS.renderAbc('score-display-1', data.tune.ABCnotation, '', {staffwidth: 650});
-    let compiled_html = template({
-        ABCnotation: data.tune.ABCnotation,
-      });
-      
-  $('.music-box').append(compiled_html);
+  //
+  // let scoreTemplate = _.template("<% ABCJS.renderAbc('score-display-numberOfScoreViews>, ABCnotation, '', {staffwidth: 650}); %>");
+  // let compiledScoreTemplate = scoreTemplate({
+  //     ABCnotation: data.tune.ABCnotation,
+  //   });
+  // $('.music-box').append(compiledScoreTemplate);
+  
+  // ABCJS.renderAbc('score-display-1', data.tune.ABCnotation, '', {staffwidth: 650});
 };
 
 const failure = (error) => {
