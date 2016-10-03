48c197c786e37fae50a2a749996944c645dbcede
DanielJRaine <ley.danielj@gmail.com>
2016-10-02 21:00:26 -0400

    Unlock the power of reading files
    Added file-reader to file-upload/events.js
    Magic ensues.  Text is returned from file on local machine.

--- assets/scripts/file-upload/events.js
+++ assets/scripts/file-upload/events.js
@@ -2,19 +2,22 @@
 const api = require('./api.js');
 const ui = require('./ui.js');
 
-// const onFileUpload = function(event) {
-//   event.preventDefault();
-//   let data;
-//   api.uploadFile(data)
-//     .done(ui.uploadFileSuccess)
-//     .fail(ui.failure);
-// };
+const onFileUpload = (event) => {
+  event.preventDefault();
+  let selectedFile = $('#file-input-upload').get(0).files[0];
+  let fileReader = new FileReader();
+  
+  fileReader.readAsText(selectedFile);
+  fileReader.onload = function(event) {
+    console.log(event.target.result);
+  };
+};
 
 const addHandlers = () => {
-  // $('#file-input-upload').on('submit', onFileUpload)
+  $('#file-button-upload').on('click', onFileUpload);
 };
 
 module.exports = {
   addHandlers,
-  // onFileUpload,
+  onFileUpload,
 };
