d516075a48de1f779a1ebf16d70303ae0347b678
DanielJRaine <ley.danielj@gmail.com>
2016-10-04 09:50:18 -0400

    Add sticky notes to DOM

--- assets/scripts/score/ui.js
+++ assets/scripts/score/ui.js
@@ -6,7 +6,6 @@ const _ = require('lodash');
 let numberOfScoreViews = 0;
 const displayScore = (data) => {
   // app.tune = data.tune;
-  debugger;
   numberOfScoreViews += 1;
   // ABCJS.renderAbc(output, tunebookString, parserParams, engraverParams, renderParams)
   // see https://github.com/paulrosen/abcjs/blob/master/api.md for documentation
@@ -16,6 +15,26 @@ const displayScore = (data) => {
   });
   $('.music-box-'+numberOfScoreViews).append(compiledMusicBoxTemplate);
   
+  // let stickyNoteTemplate = _.template(`
+  //   <div class="container bootstrap snippet">
+  //     <div class="row">
+  //     	<ul class="notes">
+  //             <li>
+  //                 <div class="rotate-1 lazur-bg id='sticky-note-<%= numberOfScoreViews %>'">
+  //                     <small>12:03:28 12-04-2014</small>
+  //                     <h4>Awesome title</h4>
+  //                     <p>These are some notes about tunes.</p>
+  //                     <a href="#" class="text-danger pull-right"><i class="fa fa-trash-o "></i></a>
+  //                 </div>
+  //             </li>
+  // 		</ul>
+  // 	</div>
+  // </div>`);
+  // let compiledStickyNoteTemplate = stickyNoteTemplate({
+  //   numberOfScoreViews: numberOfScoreViews,
+  // });
+  //
+  // $('.music-box-'+numberOfScoreViews).prepend(compiledStickyNoteTemplate);
   // let scoreTemplate = _.template("<% ABCJS.renderAbc('score-display-' + numberOfScoreViews, ABCnotation, '', {staffwidth: 650}); %>");
   // let compiledScoreTemplate = scoreTemplate({
   //     ABCnotation: data.tune.ABCnotation,
