03666eadd9ab6f862bd6b5b65d2e91fbede9d64e
danieljraine <ley.danielj@gmail.com>
2016-11-16 13:14:35 -0500

    add table image to background. get rid of bad ombre

--- assets/scripts/file-upload/api.js
+++ assets/scripts/file-upload/api.js
@@ -19,7 +19,7 @@ let tune_data = {
   keyK: '',
   rhythmR: '',
 };
-  
+
 const fileUpload = (fileText) => {
   parseFile(fileText);
   tune_data.user_id = app.user.id;
@@ -44,20 +44,20 @@ let parseFile = function(score) {
 };
 
 let matchKeys = function(element, index, array) {
-  if(element.startsWith('T')) { tune_data.tuneTitleT = element.slice(2);}
-  if(element.startsWith('C')) { tune_data.composerC = element.slice(2);}
-  if(element.startsWith('O')) { tune_data.originO = element.slice(2);}
-  if(element.startsWith('A')) { tune_data.areaA = element.slice(2);}
-  if(element.startsWith('M')) { tune_data.meterM = element.slice(2);}
-  if(element.startsWith('L')) { tune_data.unitNoteLengthL = element.slice(2);}
-  if(element.startsWith('Q')) { tune_data.tempoQ = element.slice(2);}
-  if(element.startsWith('P')) { tune_data.partsP = element.slice(2);}
-  if(element.startsWith('Z')) { tune_data.transcriptionZ = element.slice(2);}
-  if(element.startsWith('N')) { tune_data.notesN = element.slice(2);}
-  if(element.startsWith('G')) { tune_data.groupG = element.slice(2);}
-  if(element.startsWith('H')) { tune_data.historyH = element.slice(2);}
-  if(element.startsWith('K')) { tune_data.keyK = element.slice(2);}
-  if(element.startsWith('R')) { tune_data.rhythmR = element.slice(2);}
+  if(element.startsWith('T')) { tune_data.tuneTitleT = element.slice(1);}
+  if(element.startsWith('C')) { tune_data.composerC = element.slice(1);}
+  if(element.startsWith('O')) { tune_data.originO = element.slice(1);}
+  if(element.startsWith('A')) { tune_data.areaA = element.slice(1);}
+  if(element.startsWith('M')) { tune_data.meterM = element.slice(1);}
+  if(element.startsWith('L')) { tune_data.unitNoteLengthL = element.slice(1);}
+  if(element.startsWith('Q')) { tune_data.tempoQ = element.slice(1);}
+  if(element.startsWith('P')) { tune_data.partsP = element.slice(1);}
+  if(element.startsWith('Z')) { tune_data.transcriptionZ = element.slice(1);}
+  if(element.startsWith('N')) { tune_data.notesN = element.slice(1);}
+  if(element.startsWith('G')) { tune_data.groupG = element.slice(1);}
+  if(element.startsWith('H')) { tune_data.historyH = element.slice(1);}
+  if(element.startsWith('K')) { tune_data.keyK = element.slice(1);}
+  if(element.startsWith('R')) { tune_data.rhythmR = element.slice(1);}
 };
 
 const deleteTune1 = () => {
